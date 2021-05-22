ebe9855a7b688cb29ab0dc239f0055e014e5ec44
rnwind <aprokopenko84@gmail.com>
2021-05-22 18:55:48 +0300

    add predict, save/load model

--- classifier.py
+++ classifier.py
@@ -1,5 +1,6 @@
 import json
 from pathlib import Path
+import pickle
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
@@ -10,26 +11,32 @@ from src.ml.feature_extractor import FeatureExtractor
 
 class Classifier:
     def __init__(self):
-        self.labels_path = Path('data/clouds_tof.json')
-        self.pcd_path = Path('data/point_cloud_train/clouds_tof')
-        self.saved_features_path = Path('data/features.json')
-        self.X = None
-        self.y = None
+        self._labels_path = Path('data/clouds_tof.json')
+        self._pcd_path = Path('data/point_cloud_train/clouds_tof')
+        self._saved_features_path = Path('data/features.json')
+        self._model_path = 'data/model/model.pkl'
+        self._X = None
+        self._y = None
+        self.alfa_list = [5, 6, 7, 8]
+        self.voxel_downsample = None
+        self._model = None
+        if Path(self._model_path).exists():
+            self._load_model()
 
 
     def load_data_and_labels(self, alpha_list, voxel_size):
-        with self.labels_path.open() as f:
+        with self._labels_path.open() as f:
             labels_dicts = json.load(f)
         features = []
         labels = []
         to_save = {}
-        features_exist = self.saved_features_path.exists()
+        features_exist = self._saved_features_path.exists()
         if features_exist:
-            with self.saved_features_path.open() as f:
+            with self._saved_features_path.open() as f:
                 loaded_features = json.load(f)
         for idx, record in enumerate(labels_dicts):
             labels.append(record['decision'])
-            cur_pcd = str(self.pcd_path.joinpath(record['filename']))
+            cur_pcd = str(self._pcd_path.joinpath(record['filename']))
             if features_exist:
                 feature = loaded_features[record['filename']]
             else:
@@ -37,28 +44,44 @@ class Classifier:
                 to_save[record['filename']] = feature
             features.append(feature)
         if not features_exist:
-            with self.saved_features_path.open('w') as f:
+            with self._saved_features_path.open('w') as f:
                 json.dump(to_save, f, indent=4)
 
         print(f'Data loaded: {len(labels)}')
-        self.X = np.array(features)
-        self.y = np.array(labels)
-        print('x shape = ', self.X.shape)
-        print('y shape = ', self.y.shape)
+        self._X = np.array(features)
+        self._y = np.array(labels)
+        print('x shape = ', self._X.shape)
+        print('y shape = ', self._y.shape)
 
     def train(self):
-        X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, test_size=0.20, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(self._X, self._y, test_size=0.20, random_state=42)
         cls = LogisticRegression()
         cls.fit(X_train, y_train)
         y_pred = cls.predict(X_test)
         print(y_pred)
         f1 = f1_score(y_test, y_pred)
-        print(f1)
+        print('F1 score on test set:', f1)
+
+        self._model = cls
+        self._save_model()
+
+    def predict(self, pcd_path: str):
+        feature = FeatureExtractor.extract_features(pcd_path, self.alfa_list, voxel_size=self.voxel_downsample)
+        return self._model.predict([feature])
+
+    def _save_model(self):
+        with open(self._model_path, 'wb') as output:
+            pickle.dump(self._model, output)
+
+    def _load_model(self):
+        with open(self._model_path, 'rb') as input:
+            self._model = pickle.load(input)
 
 if __name__ == "__main__":
     cls = Classifier()
     cls.load_data_and_labels([5,6,7,8], None)
     cls.train()
+    print(cls.predict("data/point_cloud_train/clouds_tof/cloud_0_1620665797175109.pcd"))
 
 
 
